c4be799bac819523a5f4090b007d32102a844611
Abhishek Desai <abhishek.desai70@gmail.com>
2016-07-04 01:20:36 -0700

    Added dynamic port generation code in server.js for heroku

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 // Initializing the require modules
 var express = require('express');
-var app     = express();
+var app = express();
+
+// Create Port
+const PORT = process.env.PORT || 4000;
+
+//Create Middleware for OpenWeather API (Https/Http)
+app.use(function (req, res, next){
+  if(req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
 
 // Serve from public folder
 app.use(express.static('public'));
 
 // Creating Web Server
-app.listen(3000, function(){
-  console.log("Express Server is started on Port 3000");
-})
+app.listen(PORT, function(){
+  console.log('Express Server is started on Port ' + PORT);
+});
